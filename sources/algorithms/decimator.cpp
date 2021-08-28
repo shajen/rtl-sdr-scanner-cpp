fd477d27bb72f98e8046da04ccc899d5e62f1643
shajen <shajen@shajen.pl>
2021-08-28 23:54:55 +0200

    Improved decimator multi threading.

--- sources/algorithms/decimator.cpp
+++ sources/algorithms/decimator.cpp
@@ -1,40 +1,60 @@
 #include "decimator.h"
 
 #include <config.h>
+#include <logger.h>
 #include <utils.h>
 
-#include <thread>
+DecimatorWorker::DecimatorWorker(uint32_t rate)
+    : m_decimator(iirdecim_crcf_create_default(rate, RESAMPLER_FILTER_LENGTH)), m_isRunning(true), m_thread([this]() {
+        Logger::logger()->debug("start decimator thread");
+        while (m_isRunning) {
+          std::unique_lock<std::mutex> lock(m_mutex);
+          m_cv.wait(lock);
+          if (m_isRunning) {
+            iirdecim_crcf_execute_block(m_decimator, toLiquidComplex(m_in), m_size, toLiquidComplex(m_out));
+            m_isReady = true;
+          }
+        }
+        Logger::logger()->debug("stop decimator thread");
+      }) {}
+
+DecimatorWorker::~DecimatorWorker() {
+  m_isRunning = false;
+  m_cv.notify_one();
+  m_thread.join();
+  iirdecim_crcf_destroy(m_decimator);
+}
+
+void DecimatorWorker::decimate(std::complex<float> *in, uint32_t size, std::complex<float> *out) {
+  m_isReady = false;
+  m_in = in;
+  m_size = size;
+  m_out = out;
+  m_cv.notify_one();
+}
+
+void DecimatorWorker::waitForReady() {
+  while (!m_isReady) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  }
+}
 
 Decimator::Decimator(uint32_t rate) : m_rate(rate) {
   for (int i = 0; i < DECIMATOR_THREADS; ++i) {
-    m_decimators.push_back(iirdecim_crcf_create_default(rate, RESAMPLER_FILTER_LENGTH));
+    m_workers.push_back(std::make_unique<DecimatorWorker>(m_rate));
   }
 }
 
-Decimator::~Decimator() {
-  for (int i = 0; i < DECIMATOR_THREADS; ++i) {
-    iirdecim_crcf_destroy(m_decimators[i]);
-  };
-}
+Decimator::~Decimator() {}
 
 void Decimator::decimate(std::complex<float> *in, uint32_t size, std::complex<float> *out) {
-  if (DECIMATOR_THREADS <= 1) {
-    iirdecim_crcf_execute_block(m_decimators[0], toLiquidComplex(in), size, toLiquidComplex(out));
-    return;
+  const auto subSize = static_cast<uint32_t>(std::ceil(static_cast<float>(size) / m_workers.size()));
+  for (int i = 0; i < m_workers.size(); ++i) {
+    auto subIn = in + i * subSize * m_rate;
+    auto subOut = out + i * subSize;
+    m_workers[i]->decimate(subIn, subSize, subOut);
   }
-
-  const auto subSize = static_cast<uint32_t>(std::ceil(static_cast<float>(size) / DECIMATOR_THREADS));
-  std::vector<std::thread> threads;
-  for (int i = 0; i < DECIMATOR_THREADS; ++i) {
-    auto thread = std::thread([this, i, subSize, in, out, size]() {
-      auto subIn = in + i * subSize * m_rate;
-      auto subOut = out + i * subSize;
-      iirdecim_crcf_execute_block(m_decimators[i], toLiquidComplex(subIn), subSize, toLiquidComplex(subOut));
-    });
-    threads.push_back(std::move(thread));
-  }
-
-  for (auto &thread : threads) {
-    thread.join();
+  for (auto &worker : m_workers) {
+    worker->waitForReady();
   }
 }
--- sources/algorithms/decimator.h
+++ sources/algorithms/decimator.h
@@ -2,9 +2,36 @@
 
 #include <liquid/liquid.h>
 
+#include <atomic>
 #include <complex>
+#include <condition_variable>
 #include <cstdint>
+#include <memory>
+#include <mutex>
+#include <thread>
 #include <vector>
+
+class DecimatorWorker {
+ public:
+  DecimatorWorker(uint32_t rate);
+  ~DecimatorWorker();
+
+  void decimate(std::complex<float>* in, uint32_t size, std::complex<float>* out);
+  void waitForReady();
+
+ private:
+  iirdecim_crcf m_decimator;
+
+  std::atomic_bool m_isRunning;
+  std::atomic_bool m_isReady;
+  std::thread m_thread;
+  std::mutex m_mutex;
+  std::condition_variable m_cv;
+
+  std::complex<float>* m_in;
+  std::complex<float>* m_out;
+  uint32_t m_size;
+};
 
 class Decimator {
  public:
@@ -15,5 +42,6 @@ class Decimator {
 
  private:
   const uint32_t m_rate;
-  std::vector<iirdecim_crcf> m_decimators;
+
+  std::vector<std::unique_ptr<DecimatorWorker>> m_workers;
 };
